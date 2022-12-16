2fe394f51dbf8a5d89683b6ffd09eb96d89704e0
Viacheslav <surzhenkoviacheslav@gmail.com>
2022-12-16 15:14:06 +0200

    Update EmptyRequestBodyException.java

--- kmaooad-functions/src/main/java/edu/kmaooad/EmptyRequestBodyException.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/EmptyRequestBodyException.java
@@ -5,5 +5,13 @@ public class EmptyRequestBodyException extends Exception {
     public EmptyRequestBodyException() {
         super("Request body must not be empty");
     }
+    
+    public string GetExceptionLog(){
+        return ""
+    }
+    
+    public int GetExeptionCode(){
+        return 0
+       }
 
 }
