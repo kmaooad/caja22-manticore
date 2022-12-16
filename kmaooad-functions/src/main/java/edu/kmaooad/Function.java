293b865914f688935261b71d16ab7d8a012bdc8c
Viacheslav <surzhenkoviacheslav@gmail.com>
2022-12-16 15:12:40 +0200

    Update Function.java

--- kmaooad-functions/src/main/java/edu/kmaooad/Function.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Function.java
@@ -42,11 +42,13 @@ public class Function {
         try {
             final String requestBodyString = request.getBody().orElseThrow(EmptyRequestBodyException::new);
             
+            // TODO: Hide connection String
             ConnectionString connectionString = new ConnectionString("mongodb://manticore:SaYvqqSw7MYjW1v5@ac-duwhlzm-shard-00-00.9wxedmu.mongodb.net:27017,ac-duwhlzm-shard-00-01.9wxedmu.mongodb.net:27017,ac-duwhlzm-shard-00-02.9wxedmu.mongodb.net:27017/?ssl=true&replicaSet=atlas-tnukuv-shard-0&authSource=admin&retryWrites=true&w=majority");
             MongoClientSettings settings = MongoClientSettings.builder()
                     .applyConnectionString(connectionString)
                     .build();
             try (MongoClient mongoClient = MongoClients.create(settings)) {
+                // Conecting to manticore db
                 MongoDatabase database = mongoClient.getDatabase("manticore-db");
                 MongoCollection<Document> collection = database.getCollection("updates");
                 Document doc = Document.parse(requestBodyString);
